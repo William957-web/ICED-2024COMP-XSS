799d9f015e7edf9e71d5c07145c4299ea21bee88
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-13 22:23:50 +0800

    Update render.py

--- render.py
+++ render.py
@@ -1,6 +1,6 @@
 import selenium
 from selenium import webdriver  
-from selenium.common.exceptions import WebDriverException
+from webdriver_manager.chrome import ChromeDriverManager
 import time 
 from flask import *
 url = "https://iced-2024comp-xss.onrender.com/?content="
@@ -15,7 +15,7 @@ def main():
 @app.route('/visit',  methods=['GET', 'POST'])
 def visit():
     if request.method == 'POST':
-        opts = webdriver.FirefoxOptions()
+        opts = webdriver.ChromeOptions()
         opts.add_argument("--headless")  
         opts.add_argument("--disable-gpu") 
         opts.add_argument("--disable-extensions")
@@ -24,12 +24,15 @@ def visit():
         opts.add_argument("--disable-notifications")
         opts.add_argument('--no-sandbox')
         opts.add_argument('--disable-dev-shm-usage')
+        service = Service(executable_path=ChromeDriverManager().install())
         content=request.form['content']
         content=content.replace('%3C', '').replace('%3c', '').replace('%3E', '').replace('%3e', '').replace('<', '').replace('>', '')
-        browser = webdriver.Firefox(options=opts)
+        browser = webdriver.Firefox(service=service, options=opts)
+        browser.get(url+content)
+        time.sleep(5)
         browser.add_cookie({'name':'flag','value':'ICED{XsS_repl@c3_WAf_c4n_B33_easily_pwned}','path':'/'})
         browser.get(url+content)
-        time.sleep(15)
+        time.sleep(5)
         browser.quit()
         return "Admin will visit it!"
     else:
