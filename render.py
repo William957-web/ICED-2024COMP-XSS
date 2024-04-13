79fc81ccfcbe3585605f8731641845dc59c26d88
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-14 07:36:30 +0800

    Update render.py

--- render.py
+++ render.py
@@ -1,6 +1,5 @@
 import selenium
-from selenium import webdriver  
-from selenium.common.exceptions import WebDriverExcepti
+from selenium import webdriver
 import time 
 from flask import *
 url = "https://iced-2024comp-xss.onrender.com/?content="
@@ -15,18 +14,9 @@ def main():
 @app.route('/visit',  methods=['GET', 'POST'])
 def visit():
     if request.method == 'POST':
-        opts = webdriver.FirefoxOptions()
-        opts.add_argument("--headless")  
-        opts.add_argument("--disable-gpu") 
-        opts.add_argument("--disable-extensions")
-        opts.add_argument("--disable-infobars")
-        opts.add_argument("--start-maximized")
-        opts.add_argument("--disable-notifications")
-        opts.add_argument('--no-sandbox')
-        opts.add_argument('--disable-dev-shm-usage')
         content=request.form['content']
         content=content.replace('%3C', '').replace('%3c', '').replace('%3E', '').replace('%3e', '').replace('<', '').replace('>', '')
-        browser = webdriver.Firefox(options=opts)
+        browser = webdriver.PhantomJS(options=opts)
         browser.get(url+content)
         time.sleep(5)
         browser.add_cookie({'name':'flag','value':'ICED{XsS_repl@c3_WAf_c4n_B33_easily_pwned}','path':'/'})
