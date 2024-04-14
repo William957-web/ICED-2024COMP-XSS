71e37d5fc3d47584a9cd41331c27a623e87165fe
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-14 10:19:09 +0800

    Update render.py

--- render.py
+++ render.py
@@ -23,7 +23,11 @@ def visit():
     if request.method == 'POST':
         content=request.form['content']
         content=content.replace('%3C', '').replace('%3c', '').replace('%3E', '').replace('%3e', '').replace('<', '').replace('>', '')
-        browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
+        cap = webdriver.DesiredCapabilities.PHANTOMJS
+        cap["phantomjs.page.settings.javascriptEnabled"] = True
+        cap["phantomjs.page.settings.loadImages"] = True
+        cap["phantomjs.page.settings.userAgent"] = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+        browser = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'], desired_capabilities=cap)
         browser.get("https://iced-2024comp-xss-onrender-com.onrender.com/g3tcookieeee")
         time.sleep(5)
         browser.get(url+content)
